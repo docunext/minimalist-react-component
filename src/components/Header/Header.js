eb7c98fc733c4b51eccdb99b967b5ee7eeab4e18
Albert Lash <albert.lash@gmail.com>
2017-05-10 22:05:38 +0000

    simplifying components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,11 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
 import Link from '../Link';
 
-export class Header extends React.Component {
-    render() {
-        return (
-            <div className={s.header}>
-                <span>Simple Component</span>
-                <Link to="/">Home</Link>
-            </div>
-        );
-    }
-}
+const Header = () => (
+    <div className={s.header}>
+        <span>Simple Component</span>
+        <Link to="/">Home</Link>
+    </div>
+);
 
 export const StyledHeader = withStyles(s)(Header);
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,18 +2,13 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './HomeView.css';
 
-export class HomeView extends React.Component {
-  render() {
-    return (
-      <div className={s.giant}>
-        <h1>Hello, world!</h1>
-        <p>
-          <a href="/">Home</a>
-        </p>
-      </div>
-    );
-  };
-}
-
+const HomeView = () => (
+  <div className={s.giant}>
+    <h1>Hello, world!</h1>
+    <p>
+      <a href="/">Home</a>
+    </p>
+  </div>
+);
 
 export const StyledHomeView = withStyles(s)(HomeView);
